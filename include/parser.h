a923cb23c9bdec6ef6af42d7e3b33e909fa09255
Jan Joshua De Guzman <janjoshuadeguzman00@gmail.com>
2026-04-03 19:34:30 +0800

    fix: Updated parser to handle dynamic memory allocation.

--- include/parser.h
+++ include/parser.h
@@ -7,6 +7,6 @@
 
 #include "common.h"
 
-String* parse_arguments(String line);
+String* parse_arguments();
 
 #endif
--- src/parser.c
+++ src/parser.c
@@ -1,19 +1,39 @@
 #include "parser.h"
 
 String* parse_arguments(String line) {
-  String* arguments = (String*)malloc(sizeof(String) * BUFSIZ);
+  int arguments_capacity = BUFSIZ;
+  String* arguments = malloc(sizeof(String) * arguments_capacity);
+
+  if (arguments == NULL) {
+    exit(EXIT_FAILURE);
+  }
+
+  int position = 0;
+
   String argument = strtok(line, " ");
 
-  int argument_count = 0;
-
   while (argument != NULL) {
-    arguments[argument_count] = argument;
-    argument_count++;
+    if (position >= arguments_capacity - 1) {
+      arguments_capacity += BUFSIZ;
+
+      String* resized_arguments =
+          realloc(arguments, sizeof(String) * arguments_capacity);
+
+      if (resized_arguments == NULL) {
+        free(arguments);
+        exit(EXIT_FAILURE);
+      }
+
+      arguments = resized_arguments;
+    }
+
+    arguments[position] = argument;
+    position++;
 
     argument = strtok(NULL, " ");
   }
 
-  arguments[argument_count] = NULL;
+  arguments[position] = NULL;
 
   return arguments;
 }
